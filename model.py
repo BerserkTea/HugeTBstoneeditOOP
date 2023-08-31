cc477b2ee7bde570f30a771d0a00ec7594c2c340
Andronov Ivan (Ber Tea) <andronoviv4@gmail.com>
2023-08-31 21:27:26 +0300

    Change all for methods

--- model.py
+++ model.py
@@ -1,56 +1,83 @@
 from copy import deepcopy
 
-PATH = 'G:\GeekBrains\Developer.Prog\Python\Seminars\lst_sem\huge_phonebook\phone.txt'
-phone_book = {}
-original_book = {}
+class Contact:
+    def __init__(self, name: str, phone: str, comment: str):
+        self.name = name
+        self.phone = phone
+        self.comment = comment
 
-def open_file():
-    global phone_book, original_book, PATH
-    with open(PATH, 'r', encoding='UTF-8') as file:
-        data=file.readlines()
-    for i,contact in enumerate(data, 1):
-        contact = contact.strip().split(';')
-        phone_book[i] = contact
-    # print(phone_book)
-    original_book = deepcopy(phone_book)
+    def __str__(self):
+        return self.name, self.phone, self.comment
 
-def save_file():
-    global phone_book, PATH
-    data = []
-    for contact in phone_book.values():
-        contact = ';'.join(contact)
-        data.append(contact)
-    data = '\n'.join(data)
-    with open(PATH, 'w', encoding='UTF-8') as file:
-        file.write(data)
+    # def __str__(self):
+    #     return f'{self.name}'
+    #
+    # def __repr__(self):
+    #     return f'{self.name}'
 
-def add_contact(new_contact: list[str]):
-    global phone_book
-    c_id = max (phone_book) + 1
-    phone_book[c_id] = new_contact
+class PhoneBook:
+    def __init__(self, path: str = 'phone.txt'):
+        self.path = path
+        self.phone_book = {self, contact: Contact}
+        self.original_book = {}
 
-def find_contact(word: str) -> dict[int, list[str]]:
-    global phone_book
-    result = {}
-    for c_id, contact in phone_book.items():
-        for field in contact:
-            if word.lower() in field.lower():
-                result[c_id] = contact
-                break
-    return result
+    def open_file(self):
+        with open(self.path, 'r', encoding='UTF-8') as file:
+            data = file.readlines()
+        for i, contact in enumerate(data, 1):
+            contact = contact.strip().split(';')
+            self.phone_book[i] = Contact(*contact)
+        self.original_book = deepcopy(self.phone_book)
 
-def edit_contact(c_id: int, new_contact: list[str]):
-    global phone_book
-    current_contact = phone_book.get(c_id)
-    contact = []
-    for i in range(len(new_contact)):
-        if new_contact[i]:
-            contact.append(new_contact[i])
-        else:
-            contact.append(current_contact[i])
-    phone_book[c_id] = contact
-    return new_contact[0]
+    def save_file(self):
+        data = []
+        for contact in self.phone_book.values():
+            contact = ';'.join(contact)
+            data.append(contact)
+        data = '\n'.join(data)
+        with open(self.path, 'w', encoding='UTF-8') as file:
+            file.write(data)
 
-def delete_contact(c_id: int) ->str:
-    global phone_book
-    return phone_book.pop(c_id)[0]
+    def add_contact(self, new_contact: list[str]):
+        c_id = max(self.phone_book) + 1
+        self.phone_book[c_id] = new_contact
+
+    def find_contact(self, word: str) -> dict[int, list[str]]:
+        result = {}
+        print(self.phone_book)
+        for c_id, contact in self.phone_book.items():
+            print(type(contact))
+            for field in contact:
+                if word.lower() in field.lower():
+                    result[c_id] = contact
+                    break
+        return result
+
+    def edit_contact(self, c_id: int, new_contact: list[str]):
+        current_contact = self.phone_book.get(c_id)
+        contact = []
+        for i in range(len(new_contact)):
+            if new_contact[i]:
+                contact.append(new_contact[i])
+            else:
+                contact.append(current_contact[i])
+        self.phone_book[c_id] = Contact(*contact)
+        return new_contact[0]
+
+    def delete_contact(self, c_id: int) ->str:
+        return self.phone_book.pop(c_id)[0]
+
+    def max_len_name(self, option: str) -> int:
+        result = []
+        # print(self.phone_book)
+        for contact in self.phone_book.values():
+            if option == 'name':
+                item = contact.name
+            elif option == 'phone':
+                item = contact.phone
+            else:
+                item = contact.comment
+            result.append(item)
+        return len(max(result, key=len))
+
+
--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 import text
+from model import Contact, PhoneBook
 
 
 def main_menu():
@@ -21,14 +22,18 @@ def print_message(msg: str):
     print(msg)
     print('='*len(msg) + '\n')
 
-def show_book (book: dict[int, list[str]], msg:str):
+
+def show_book (book: PhoneBook, msg: str):
     if book:
-        print('\n' + '*'* 67)
-        for i, contact in book.items():
-            print(f'{i:>3}. {contact[0]:<20} {contact[1]:<20} {contact[2]:<20}')
-        print('*'*67 + '\n')
+        print('\n' + '*' * (book.max_len_name("name") + book.max_len_name("phone") + book.max_len_name("comment")+8))
+        for i, contact in book.phone_book.items():
+            print(f'{i:>3}. {contact.name:<{book.max_len_name("name")}}'
+                  f' {contact.phone:<{book.max_len_name("phone")}}'
+                  f' {contact.comment:<{book.max_len_name("comment")}}')
+        print('*' * (book.max_len_name("name") + book.max_len_name("phone") + book.max_len_name("comment") + 8)+'\n')
     else:
         print_message(msg)
+
 
 def input_contact(msg: str)-> list[str]:
     contact = []
